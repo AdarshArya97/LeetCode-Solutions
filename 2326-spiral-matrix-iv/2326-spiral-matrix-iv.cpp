53fe221a761d889503449c2495abcca2f6124890
Adarsh Arya <146611146+AdarshArya97@users.noreply.github.com>
2026-09-22 16:43:29 +0530

    Time: 13 ms (91.93%), Space: 138.2 MB (32.82%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> matrix(m, vector<int>(n,-1));
+        int minr = 0 , minc = 0;
+        int maxr = m-1 , maxc = n-1;
+        ListNode* temp = head;
+        while(minr<=maxr && minc<=maxc){
+            for(int j= minc ; j<=maxc  ; j++){
+                if(temp==NULL) return matrix;
+                matrix[minr][j]=temp->val;
+                temp = temp->next;
+            }
+            minr++;
+            if(minr>maxr || minc>maxc) break;
+            for(int i = minr ; i<=maxr; i++){
+                if(temp==NULL) return matrix;
+                matrix[i][maxc]=temp->val;
+                temp = temp->next;
+            }
+            maxc--;
+            if(minr>maxr || minc>maxc) break;
+            for(int j = maxc ; j>=minc ; j--){
+                if(temp==NULL) return matrix;
+                matrix[maxr][j]=temp->val;
+                temp = temp->next;
+            }
+            maxr--;
+            if(minr>maxr || minc>maxc) break;
+            for(int i = maxr; i>=minr  ; i--){
+                if(temp==NULL) return matrix;
+                matrix[i][minc]=temp->val;
+                temp = temp->next;
+            }
+            minc++;
+        }
+        return matrix;
+    }
+};
